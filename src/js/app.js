f0cd733ab77f3464a09bebdb2811d058e54d4650
Kristiyan <javakris95@gmx.com>
2022-12-07 13:22:05 +0200

    add span

--- src/js/app.js
+++ src/js/app.js
@@ -3,5 +3,12 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
-
+  var div = document.querySelector('.product')
+  var price = document.querySelectorAll('p')[1].innerHTML;
+  var span = document.createElement('span')
+  span.class = 'data-price'
+  span.className = 'data-price'
+  span.innerHTML = price
+  span.setAttribute('data-price', price)
+  div.appendChild(span)
 });
